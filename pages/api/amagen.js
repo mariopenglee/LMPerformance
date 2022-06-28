785d25170f95b2ddc7bc7cfadffc6d23f45d78b7
MariusPL <mario.penglee.mpl@gmail.com>
2022-06-28 17:37:13 -0300

    api key restore

--- pages/api/amagen.js
+++ pages/api/amagen.js
@@ -1,7 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-    apiKey: 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,
+    apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
